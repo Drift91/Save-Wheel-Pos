1bed5abd4da9fced1a81e6ed39ebedc23b79eb46
Kyle J. McKeown <Drift91@users.noreply.github.com>
2025-11-15 19:45:06 -0500

    Update client.lua
    Should now work when engine is running.

--- savewheelpos/client.lua
+++ savewheelpos/client.lua
@@ -1,6 +1,7 @@
 CreateThread(function()
     local angle = 0.0
     local speed = 0.0
+	
     while true do
         Wait(50)
 		local ped = PlayerPedId()
@@ -11,10 +12,15 @@ CreateThread(function()
                 angle = tangle
             end
             speed = GetEntitySpeed(veh)
-            local lastVeh = GetVehiclePedIsIn(ped, true)
-            if speed < 0.1 and DoesEntityExist(lastVeh) and not GetIsTaskActive(ped, 151) and not GetIsVehicleEngineRunning(lastVeh) then
-                SetVehicleSteeringAngle(lastVeh, angle)
-            end
-        end
+		end
+
+		local lastVeh = GetVehiclePedIsIn(ped, true)
+
+		local isOffOrDismount = not GetIsVehicleEngineRunning(lastVeh) or lastVeh ~= veh or GetIsTaskActive(ped, 2)
+		if speed < 0.1 and DoesEntityExist(lastVeh) and isOffOrDismount and not IsThisModelABike(GetEntityModel(lastVeh)) then
+
+			SetVehicleSteeringAngle(lastVeh, angle)
+		end
+		
     end
 end)
