6d8960397aa666c10f69253a50f7bda297690634
Kyle J. McKeown <Drift91@users.noreply.github.com>
2025-11-15 19:44:00 -0500

    Update client.lua

--- savewheelpos/client.lua
+++ savewheelpos/client.lua
@@ -1,18 +1,19 @@
-Citizen.CreateThread(function()
+CreateThread(function()
     local angle = 0.0
     local speed = 0.0
     while true do
-        Citizen.Wait(0)
-        local veh = GetVehiclePedIsUsing(PlayerPedId())
+        Wait(50)
+		local ped = PlayerPedId()
+        local veh = GetVehiclePedIsUsing(ped)
         if DoesEntityExist(veh) then
             local tangle = GetVehicleSteeringAngle(veh)
             if tangle > 10.0 or tangle < -10.0 then
                 angle = tangle
             end
             speed = GetEntitySpeed(veh)
-            local vehicle = GetVehiclePedIsIn(PlayerPedId(), true)
-            if speed < 0.1 and DoesEntityExist(vehicle) and not GetIsTaskActive(PlayerPedId(), 151) and not GetIsVehicleEngineRunning(vehicle) then
-                SetVehicleSteeringAngle(GetVehiclePedIsIn(PlayerPedId(), true), angle)
+            local lastVeh = GetVehiclePedIsIn(ped, true)
+            if speed < 0.1 and DoesEntityExist(lastVeh) and not GetIsTaskActive(ped, 151) and not GetIsVehicleEngineRunning(lastVeh) then
+                SetVehicleSteeringAngle(lastVeh, angle)
             end
         end
     end
